b26ea81cf19d96c8e7bcdc1d9e2c4026cbed2ede
Rahul <rahu8299@gmail.com>
2026-01-18 14:06:58 +0530

    adding prerender checks

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,43 @@ export default function prerenderStaticPlugin(
   options: Omit<PrerenderOptions, "template" | "dist"> & {
     template?: string;
     dist?: string;
-  }
+  },
 ): Plugin {
   return {
     name: "vite-plugin-prerender-static",
     apply: "build",
 
     closeBundle() {
+      // add error handling
+      if (!fs.existsSync("index.html")) {
+        console.warn("Missing index.html — skipping prerender");
+        console.warn("Make sure to build your project before prerendering.");
+        return;
+      }
+      if (options.dist && !fs.existsSync(options.dist)) {
+        console.warn(`Missing ${options.dist} — skipping prerender`);
+        console.warn("Make sure to build your project before prerendering.");
+        return;
+      } else if (!options.dist && !fs.existsSync("dist")) {
+        console.warn("Missing dist/ — skipping prerender");
+        console.warn("Make sure to build your project before prerendering.");
+        return;
+      }
+
+      if (options.routes && options.routes.length === 0) {
+        console.warn("No routes specified — skipping prerender");
+        console.warn("Make sure to provide at least one route to prerender.");
+        return;
+      }
+
+      if (!options.template && !fs.existsSync("template.html")) {
+        console.warn("Missing template.html — skipping prerender");
+        console.warn(
+          "Make sure to provide a template file. See README for details.",
+        );
+        return;
+      }
+
       const dist = options.dist ?? path.resolve(process.cwd(), "dist");
       const html = fs.readFileSync(path.join(dist, "index.html"), "utf8");
       const template =
